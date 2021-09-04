dd8ad3c2cce45ef35737fb908b83a01e7a7ad4d3
Rafael <ra.facavalcante@hotmail.com>
2021-09-03 22:54:34 -0300

    classe-01/query:

--- classe-01/query.sql
+++ classe-01/query.sql
@@ -1,1 +1,23 @@
+CREATE database orquestra;
 
+select compositor, id from musicas;
+
+select composicao, tempo from musicas;
+
+select compositor, composicao, id from musicas where id between 47 and 123;
+
+select * from musicas where compositor is not null and tempo < 240 and compositor != 'Bach';
+
+select composicao, tempo from musicas where compositor = 'Mozart' or compositor = 'Bach';
+
+select * from musicas order by id desc;
+
+select * from musicas order by tempo desc;
+
+select * from musicas order by tempo limit 5;
+
+select * from musicas order by tempo limit 10 offset 5;
+
+select * from musicas limit 10 offset 40;
+
+select * from musicas limit 12 offset 72;
